0f7670aece5c3fdaeddc665af256021a880f6c11
flppereira <fe.pereirajj@gmail.com>
2025-02-25 01:23:47 +0000

    ficando loco

--- CartasSupertTrunfo.c
+++ CartasSupertTrunfo.c
@@ -5,12 +5,12 @@ int main(){
     // Declaração de Variaveis.
 
 char estado1, estado2;
-char codigo_da_carta1[50], codigo_da_carta2[50];
+char codigodacarta1[3], codigo_da_carta2[3];
 char nome_da_cidade1[50], nome_da_cidade2[50];
 int populacao1, populacao2;
 float area_km1, area_km2;
 float pib1, pib2;
-int pontos_turisticos1, pontos_turisticos2;
+int turisticos1, turisticos2;
 
 // Mensagem de boas vindas
 printf("Bem vindo ao Cadastro de Cartas do Jogo Super Trunfo!\n");
@@ -20,8 +20,8 @@ printf("---- CARTA 1 -----");
 printf("Digite a inicial do Estado (A-H): \n");
 scanf("%c", &estado1);
 
-printf("Digite o código da carta(inicial do estado + 01-04)\n");
-scanf("%s", &codigo_da_carta1);
+printf("Digite o código da carta(inicial do estado + 01-04): \n");
+scanf("%c", &codigodacarta1);
 
 printf("Digite o nome da cidade: \n");
 scanf("%s", &nome_da_cidade1);
@@ -32,11 +32,11 @@ scanf("%d", &populacao1);
 printf("Digite a area (em km²): \n");
 scanf("%f", &area_km1 );
 
-printf("Digite o PIB da cidade: \n";)
+printf("Digite o PIB da cidade: \n");
 scanf("%f", pib1);
 
-printf ("Digite o numero de pontos turisticos da cidade: \n");
-scanf("%d", pontos_turisticos1);
+printf("Digite o numero de pontos turisticos da cidade: \n");
+scanf("%d", &turisticos1);
 
 
 
@@ -44,7 +44,7 @@ printf("---- CARTA 2 -----");
 printf("Digite a inicial do Estado (A-H): \n");
 scanf("%c", &estado2);
 
-printf("Digite o código da carta(inicial do estado + 01-04)\n");
+printf("Digite o código da carta(inicial do estado + 01-04): \n");
 scanf("%s", &codigo_da_carta2);
 
 printf("Digite o nome da cidade: \n");
@@ -56,13 +56,12 @@ scanf("%d", &populacao2);
 printf("Digite a area (em km²): \n");
 scanf("%f", &area_km2);
 
-printf("Digite o PIB da cidade: \n";)
+printf("Digite o PIB da cidade: \n");
 scanf("%f", pib2);
 
-printf ("Digite o numero de pontos turisticos da cidade: \n");
-scanf("%d", pontos_turisticos2);
+printf("Digite o numero de pontos turisticos da cidade: \n");
+scanf("%d", &turisticos2);
 }
-
 
 
 
